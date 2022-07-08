5559acc4747ac04d1511b819815c26cb9835e24d
Bran <brane22lp@gmail.com>
2022-07-08 17:53:27 -0500

    subo primer ejercicio de prueba

---
+++ Primer Parcial Ejercicio 1.c
@@ -0,0 +1,34 @@
+#include stdio.h
+    int a;
+    void eleccion();
+    void factorial();
+    
+    void main(){
+        eleccion();
+        int c;
+         c=a;
+        factorial();
+        printf(El factorial de %i es %i,c,a);
+    return 0;
+    }
+
+    eleccion(){
+        printf(por favor, ingresa un numero del 1 al 5 );
+        scan    f(%i,&a);
+        if (a=5 && a=1){
+        return a;
+        }
+        else
+        printf(El numero ingresado no es valido, intenta de nuevo n);
+        eleccion();
+    }
+
+    factorial(){
+        int b;
+        b = a;
+        while (b1){
+        b = b-1;
+        a= ab;
+        }
+        return a;
+    }
